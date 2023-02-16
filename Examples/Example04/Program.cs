99ac681f1e0e117b617aedb1f3ede75567eb8b43
Niuta <Niuta_87@mail.ru>
2023-02-16 22:15:19 +0600

    Example max three numbers

---
+++ Examples/Example04/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+
+Console.Write("Введи число а:  ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(max<numberB)
+{
+              max = numberB;
+}
+if(max<numberC)
+{
+              max = numberC;
+}
+
+Console.Write("max = ");
+Console.Write(max);
